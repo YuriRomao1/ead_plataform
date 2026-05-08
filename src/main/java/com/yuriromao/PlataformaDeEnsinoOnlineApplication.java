8416c7c87ee0068e3a2521c59bdf7a67f7d67d78
Yuri Romão <yurijj@hotmail.com>
2026-05-08 16:32:57 -0300

    first commit

---
+++ src/main/java/com/yuriromao/PlataformaDeEnsinoOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.yuriromao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDeEnsinoOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaDeEnsinoOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuriromao/PlataformaDeEnsinoOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuriromao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDeEnsinoOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
